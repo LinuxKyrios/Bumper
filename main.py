214d5fd078249670e668d10adbfd91b6a4865f66
Linux Kyrios <malinowskim92@protonmail.com>
2021-06-12 10:21:21 +0200

    Added comments to main.py file

--- main.py
+++ main.py
@@ -1,18 +1,24 @@
 import discord, os, time
 from discord.ext import commands
 
+#setting prefix for script
 bot = commands.Bot(command_prefix = "--", self_bot=True)
 
+#Console confirmation that script is working
 @bot.event
 async def on_ready():
-    print("Linux Kyrios Bumper is Online!")
+    print("Linux Kyrios Bumper is OnLine!")
 
+#Command to execute on discord
 @bot.command()
-async def bumper(ctx):
+async def bump(ctx):
     while True:
         await ctx.send("!d bump")
-        time.sleep(7260)
+        print("Bump sent")
+        time.sleep(7200)
 
-token = "Nzc3NDc4ODQ1ODc5NTQ5OTUy.YMOo2g.4SfjBkr-VYULAkfZ8senkUSL690"
+#token is necessary to connect with accounton discord
+token = "Nzc3NDc4ODQ1ODc5NTQ5OTUy.YMRMBQ.s37MyzIMGuV_jsAcCg7pC7Dscm8"
 
+#execute the script
 bot.run(token, bot = False)
